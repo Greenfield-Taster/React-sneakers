72813ee9e563a72bc61a08e974a6c60adf45d7e7
Asya Gorbatchova <gorbatchovaasa123@gmail.com>
2023-05-27 18:54:08 +0300

    Fix connection with Card styles by props

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,21 +1,22 @@
-import styles from "./Card.modul.css";
+import React from "react";
+import styles from "./Card.module.css";
 
 console.log(styles);
 
 function Card(props) {
   return (
-    <div class="card">
-      <div className="favorite" onClick={props.onFavorite}>
-        <img width={20} height={20} src={props.liked} alt="Unliked" />
+    <div className={styles.card}>
+      <div className={styles.favorite} onClick={props.onFavorite}>
+        <img src={props.liked} alt="Unliked" />
       </div>
-      <img class="displayed" src={props.imageUrl} alt="Sneakers" />
+      <img class={styles.displayed} src={props.imageUrl} alt="Sneakers" />
       <h5>{props.title}</h5>
-      <div className="cardButton">
-        <div className="textButton">
+      <div className={styles.cardButton}>
+        <div className={styles.textButton}>
           <span>Цена:</span>
           <b style={{ fontSize: 13 }}>{props.price} грн</b>
         </div>
-        <button className="button" onClick={props.onPlus}>
+        <button className={styles.button} onClick={props.onPlus}>
           <img width={11} height={11} src={props.plus} alt="Plus" />
         </button>
       </div>
